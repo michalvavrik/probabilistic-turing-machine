dd8dcb3b8093a16a761c93429ce34049ab766471
Michal Vavřík <mvavrik@redhat.com>
2023-01-20 15:17:09 +0100

    Fermat Primality test WIP

--- src/test/java/edu/michalvavrik/ptm/FermatPrimalityTest.java
+++ src/test/java/edu/michalvavrik/ptm/FermatPrimalityTest.java
@@ -12,7 +12,21 @@ public class FermatPrimalityTest {
         final var cmd = new StartCommand();
         cmd.setTransitionFunction("src/test/resources/fermat-primality-test.txt");
         cmd.converseDecimalToBinary = true;
-        final int primeCandidate = 5;
+        verify(cmd, 3);
+        verify(cmd, 4);
+        verify(cmd, 5);
+        verify(cmd, 6);
+        verify(cmd, 7);
+        verify(cmd, 8);
+        verify(cmd, 9);
+        verify(cmd, 10);
+        verify(cmd, 11);
+        verify(cmd, 12);
+        verify(cmd, 13);
+        verify(cmd, 14);
+    }
+
+    private void verify(StartCommand cmd, int primeCandidate) {
         cmd.setInputData(Integer.toString(primeCandidate));
         final var result = cmd.computeInputData().tape();
         Assertions.assertEquals(1, result.length);
